3afb5a6a6c61f1c48a332305882986bcd0f0344d
Kktbots <143538428+Kktbots@users.noreply.github.com>
2023-10-20 22:23:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6692879342:AAGxaRJhlHIn_A5Iw-rcdOYPC1fyJF7z8iM",
-             api_id= 29540156,
-             api_hash= "775d47859519f34f01549344a392280b")
+             bot_token= ""
+             api_id= ,
+             api_hash= "")
 
 
 @bot.on_message(filters.command(["start"]) & filters.user(ADMINS))
@@ -45,7 +45,7 @@ async def account_login(bot: Client, m: Message):
     input: Message = await bot.listen(editable.chat.id)
     if input.document:
         x = await input.download()
-        await bot.send_document(-1001974000179, x)
+        await bot.send_document(-, x)
         await input.delete(True)
         file_name, ext = os.path.splitext(os.path.basename(x))
         credit = f"[{m.from_user.first_name}](tg://user?id={m.from_user.id})"
@@ -175,7 +175,7 @@ async def account_login(bot: Client, m: Message):
                     try:
                         ka = await helper.download(url, name)
                         copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc1)
-                        await copy.copy(chat_id = -1001974000179)
+                        await copy.copy(chat_id = -)
                         count+=1
                         os.remove(ka)
                         time.sleep(1)
@@ -189,7 +189,7 @@ async def account_login(bot: Client, m: Message):
                         download_cmd = f"{cmd} -R 25 --fragment-retries 25"
                         os.system(download_cmd)
                         copy = await bot.send_document(chat_id=m.chat.id,document=f'{name}.pdf', caption=cc1)
-                        await copy.copy(chat_id = -1001974000179)
+                        await copy.copy(chat_id = -)
                         count += 1
                         os.remove(f'{name}.pdf')
                     except FloodWait as e:
