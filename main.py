199f5b1b9ebfd72a61fa6c2c531de1299c71c245
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-10-23 17:48:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -175,7 +175,7 @@ async def account_login(bot: Client, m: Message):
                     try:
                         ka = await helper.download(url, name)
                         copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc1)
-                        await copy.copy(chat_id = -)
+                        await copy.copy(chat_id = -1002029805707)
                         count+=1
                         os.remove(ka)
                         time.sleep(1)
